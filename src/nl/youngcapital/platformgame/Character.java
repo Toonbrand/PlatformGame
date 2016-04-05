fbca5d8f9586d6459fd8bfff18bf4dd2f20efaf7
Samuel Rundengan <rockman_vforce@hotmail.com>
2016-04-05 16:58:38 +0200

    character

---
+++ src/nl/youngcapital/platformgame/Character.java
@@ -0,0 +1,16 @@
+package nl.youngcapital.platformgame;
+
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+
+public class Character {
+	
+	int posX = 0;
+	int posY = 0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
