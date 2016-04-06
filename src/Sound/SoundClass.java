7aeb3c14d42894ac38aae78972d569ae5e59fd86
Samuel Rundengan <rockman_vforce@hotmail.com>
2016-04-06 15:03:19 +0200

    NYAN NYAN WERKT

--- src/Sound/SoundClass.java
+++ src/Sound/SoundClass.java
@@ -1,18 +1,24 @@
 package Sound;
 
+import javafx.embed.swing.JFXPanel;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+
+
 
 public class SoundClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFXPanel fxPanel = new JFXPanel();
+
+		SoundClass s = new SoundClass();
 		
-		String bip = "Nyan.mp3";
+		String bip = s.getClass().getResource("/Sound/Nyan.mp3").toString();
+		System.out.println(bip);
 		Media hit = new Media(bip);
 		MediaPlayer mediaPlayer = new MediaPlayer(hit);
 		mediaPlayer.play();
-
+		
 	}
-
 }
+
